0e1ad7da78106d5eeb98ffbc0f01d90e16cb2d3f
Mike Norris <mnorris6@elon.edu>
2023-09-20 23:57:22 +0300

    removed window client id script tag test

--- src/Components/PortfolioNav.js
+++ src/Components/PortfolioNav.js
@@ -59,6 +59,8 @@ export default function PortfolioNav() {
 
   const navigateTo = (page) => {
     navigate(`/portfolio/${page.toLowerCase()}`);
+    handleCloseUserMenu()
+    handleCloseNavMenu()
   };
 
   const getCartLength = () => {
@@ -112,17 +114,7 @@ export default function PortfolioNav() {
 </Menu>
 </Box>
 
-{/* <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-{pages.map((page) => (
-  <Button
-    key={page}
-    onClick={() => navigateTo(page)}
-    sx={{ my: 2, color: 'white', display: 'block' }}
-  >
-    {page}
-  </Button>
-))}
-</Box> */}
+
         <LaptopChromebookIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
         <Typography
             variant="h6"
@@ -158,26 +150,9 @@ export default function PortfolioNav() {
               textDecoration: "none",
             }}
           >
-            Script for Java
+            E-Commerce
           </Typography>
 
-          {/* <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="#/stackathon/trip"
-            sx={{
-              mr: 2,
-              display: { xs: "none", md: "flex" },
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            Stackathon
-          </Typography> */}
           <SportsKabaddiIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
           <Typography
             variant="h6"
@@ -254,76 +229,6 @@ export default function PortfolioNav() {
             C-Trade
           </Typography>
           < SearchBar />
-          
-
-          {/* <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={() => navigateTo(`java/${page}`)}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box> */}
-
-          {/* <EmojiFoodBeverageIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href=""
-            sx={{
-              mr: -5,
-              display: { xs: "flex", md: "none" },
-              flexGrow: 1,
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".2rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            Script for Java
-          </Typography> */}
-
-          {/* <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={() => navigateTo(`java/${page}`)}
-                sx={{ my: 2, color: "white", display: "block" }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box> */}
           
 
           <Typography
