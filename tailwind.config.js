4cb3041d8f6b49efcb98e5e85768d55b308699c1
Mike Norris <mnorris6@elon.edu>
2023-12-03 17:06:35 -0500

    build test 10 added tailwind derivs to styles.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   important: true,
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js, jsx}"],
   theme: {
     extend: {},
   },
