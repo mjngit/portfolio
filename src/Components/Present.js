518b171652bf7436456797600fa61a0f7ad074b2
Mike Norris <mnorris6@elon.edu>
2023-12-04 20:52:10 -0500

    brute forcing style all 2.4

--- src/Components/Present.js
+++ src/Components/Present.js
@@ -99,13 +99,13 @@ setLoading(false)
                   <Button onClick={ submit } disabled={ !age || !minPrice || !maxPrice || !interest1 || !interest2}>Generate Birthday Present</Button>
                 </form> */}
 
-                <div className="grid max-w-screen-xl grid-cols-1 gap-8 px-8 py-16 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32" style={{display: 'grid', gridTemplateColumns: 'repeat(2, minmax(0, 1fr))', maxWidth: '720px', gap: '2rem', paddingTop: '2rem', paddingBottom: '4rem', paddingLeft: '2rem', paddingRight: '2rem', borderRadius: '1rem'}}>
-                    <div className="flex flex-col justify-between" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
-                        <div className="space-y-2" style={{ paddingTop: '.5rem', paddingBottom: '.5rem'}}>
+                <div className="grid max-w-screen-xl grid-cols-1 gap-8 px-8 py-16 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32" style={{display: 'grid', gridTemplateColumns: 'repeat(3, minmax(0, 1fr))', maxWidth: '1280px', gap: '2rem', paddingTop: '2rem', paddingBottom: '2rem', paddingLeft: '4rem', paddingRight: '4rem', borderRadius: '1rem'}}>
+                    <div className="flex flex-col justify-between" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
+                        <div className="space-y-2" style={{ justifyContent: 'space-evenly', paddingTop: '.5rem', paddingBottom: '.5rem'}}>
                             <h2 className="text-4xl font-bold leadi lg:text-5xl" style={{ fontSize: '2.25rem', lineHeight: '2.5rem', fontWeight: 'bold' }}>Drawing a Blank?</h2>
                             <div className="dark:text-gray-400">Fill in the info about your friend or loved one and let AI do the rest! Keep in mind the present links are likely outdated but the ideas remain top notch and great starting points for your brainstorm!</div>
                         </div>
-                        <img src="static/images/cakepresents.png" alt="Birthday Cake & Presents" className="p-6 h-52 md:h-64" style={{ padding: '1.5rem', height: '12rem'}} />
+                        <img src="static/images/cakepresents.png" alt="Birthday Cake & Presents" className="p-6 h-52 md:h-64" style={{ padding: '1.5rem',height: '12rem'}} />
                     </div>
                     <form onSubmit={ submit } novalidate="" className="space-y-6" style={{ marginLeft:' 1.5rem'}} >
                         <div>
