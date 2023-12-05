215360ac85ebf4db3801d74038472df252e2d86a
Mike Norris <mnorris6@elon.edu>
2023-12-04 22:13:11 -0500

    brute forcing style all 2.7

--- src/Components/Present.js
+++ src/Components/Present.js
@@ -100,14 +100,14 @@ setLoading(false)
                 </form> */}
 
                 <div className="grid max-w-screen-xl grid-cols-1 gap-8 px-8 py-16 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32" style={{display: 'grid', gridTemplateColumns: 'repeat(3, minmax(0, 1fr))', maxWidth: '1280px', gap: '2rem', paddingTop: '2rem', paddingBottom: '2rem', paddingLeft: '4rem', paddingRight: '4rem', borderRadius: '1rem'}}>
-                    <div className="flex flex-col justify-between" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', width: '15rem'}}>
+                    <div className="flex flex-col justify-between" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', width: '35rem'}}>
                         <div className="space-y-2" style={{ justifyContent: 'space-evenly', paddingTop: '.5rem', paddingBottom: '.5rem'}}>
                             <h2 className="text-4xl font-bold leadi lg:text-5xl" style={{ fontSize: '2.25rem', lineHeight: '2.5rem', fontWeight: 'bold' }}>Drawing a Blank?</h2>
                             <div className="dark:text-gray-400">Fill in the info about your friend or loved one and let AI do the rest! Keep in mind the present links are likely outdated but the ideas remain top notch and great starting points for your brainstorm!</div>
                         </div>
                         <img src="static/images/cakepresents.png" alt="Birthday Cake & Presents" className="p-6 h-52 md:h-64" style={{ padding: '1.5rem',height: '12rem'}} />
                     </div>
-                    <form onSubmit={ submit } novalidate="" className="space-y-6" style={{ marginLeft:' 1.5rem', width: '15rem'}} >
+                    <form onSubmit={ submit } novalidate="" className="space-y-6" style={{ marginLeft:' 1.5rem', width: '35rem'}} >
                         <div>
                             <label for="name" className="text-sm" style={{fontSize: '0.875rem', lineHeight: '1.25rem'}}>Age</label>
                             <input value={ age } onChange={ev => setAge(ev.target.value)} id="name" type="text" placeholder="" className="w-full p-3 rounded dark:bg-gray-800 text-white" style={{ width: '100%', padding: '1rem', borderRadius: '.25rem'}} />
