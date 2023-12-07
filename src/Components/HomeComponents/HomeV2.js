cdd983ebfa11106554fa66d55bf3fdbd91d08d58
Mike Norris <mnorris6@elon.edu>
2023-12-06 19:21:48 -0500

    brute styling 1.25

--- src/Components/HomeComponents/HomeV2.js
+++ src/Components/HomeComponents/HomeV2.js
@@ -163,8 +163,8 @@ function HomeV2() {
                 </div>
             </section>
             <section className="py-6 dark:bg-gray-800 dark:text-gray-50" style={{ paddingTop: '1.5rem', paddingBottom: '1.5rem' }}>
-                <div className="container flex flex-col justify-center p-4 mx-auto space-y-8 md:p-10 lg:space-y-0 lg:space-x-12 lg:justify-between lg:flex-row" style={{ display: 'flex', flexDirection: 'column', padding: ".75rem", marginLeft: '2rem', alignItems: 'center' }}>
-                    <div className="flex flex-col space-y-4 text-center lg:text-left" style={{ display: 'flex', flexDirection: 'row', padding: ".75rem", marginLeft: '2rem', textAlign: 'left' }}>
+                <div className="container flex flex-col justify-center p-4 mx-auto space-y-8 md:p-10 lg:space-y-0 lg:space-x-12 lg:justify-between lg:flex-row" style={{ display: 'flex', flexDirection: 'row', padding: ".75rem", marginLeft: '2rem', alignItems: 'center' }}>
+                    <div className="flex flex-col space-y-4 text-center lg:text-left" style={{ display: 'flex', flexDirection: 'column', padding: ".75rem", marginLeft: '2rem', textAlign: 'left' }}>
                         <h1 className="text-5xl font-bold leadi">Connect with me on LinkedIn</h1>
                         <p className="text-lg" >Connect with me and shoot me a message to talk about anything and everything!</p>
                     </div>
@@ -191,8 +191,8 @@ function HomeV2() {
                             <p>This is my capstone project where I worked with 2 other people to design and build a trading community to research, discuss, buy, and sell stocks. I was the lead engineer on this project.</p>
                         </div>
                     </a>
-                    <div className="grid justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3" style={{ display: 'grid', justifyContent: 'center', gap: '2rem', gridTemplateColumns: '450px, 450px ' }}>
-                        <a rel="noopener noreferrer" href="#/stackathon/fighters" className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-900" style={{ maxWidth: '24rem' }}>
+                    <div className="grid justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3" style={{ display: 'grid', justifyContent: 'center', gap: '2rem', gridTemplateColumns: '450px, 450px' }}>
+                        <a rel="noopener noreferrer" href="#/stackathon/fighters" className="max-w-sm mx-auto group hover:no-underline focus:no-underline dark:bg-gray-900" style={{ width: '350px' }}>
                             <img role="presentation" className="object-cover w-full rounded h-44 dark:bg-gray-500" src="/static/images/fighter-stats.jpeg" style={{ objectFit: 'cover', width: '100%', borderRadius: '0.25rem', height: '12rem'   }} />
                             <div className="p-6 space-y-2" style={{ padding: '1.5rem' }}>
                                 <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline" style={{ fontSize: '1.5rem', lineHeight: '2rem', fontWeight: '600'}}>UFC Stat Tool</h3>
@@ -336,8 +336,8 @@ function HomeV2() {
                         </li>
                     </ul>
                     <div className="flex flex-col justify-center pt-6 lg:pt-0" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-end', paddingTop: '1.5rem' }} >
-                        <div className="flex justify-center space-x-4" style={{ display: 'flex', justifyContent: 'center', paddingLeft: '2rem' }}>
-                            <a rel="noopener noreferrer" href="https://www.instagram.com/numerounoabuelo/" className="flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 dark:bg-lime-400 dark:text-gray-900" style={{ paddingLeft: '2rem'}}>
+                        <div className="flex justify-center space-x-4" style={{ display: 'flex', justifyContent: 'flex-end', paddingLeft: '2rem' }}>
+                            <a rel="noopener noreferrer" href="https://www.instagram.com/numerounoabuelo/" className="flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 dark:bg-lime-400 dark:text-gray-900" style={{ paddingLeft: '8px'}}>
                                 <InstagramIcon />
                             </a>
                             {/* Pinterest */}
@@ -352,7 +352,7 @@ function HomeV2() {
                                     <path d="M31.937 6.093c-1.177 0.516-2.437 0.871-3.765 1.032 1.355-0.813 2.391-2.099 2.885-3.631-1.271 0.74-2.677 1.276-4.172 1.579-1.192-1.276-2.896-2.079-4.787-2.079-3.625 0-6.563 2.937-6.563 6.557 0 0.521 0.063 1.021 0.172 1.495-5.453-0.255-10.287-2.875-13.52-6.833-0.568 0.964-0.891 2.084-0.891 3.303 0 2.281 1.161 4.281 2.916 5.457-1.073-0.031-2.083-0.328-2.968-0.817v0.079c0 3.181 2.26 5.833 5.26 6.437-0.547 0.145-1.131 0.229-1.724 0.229-0.421 0-0.823-0.041-1.224-0.115 0.844 2.604 3.26 4.5 6.14 4.557-2.239 1.755-5.077 2.801-8.135 2.801-0.521 0-1.041-0.025-1.563-0.088 2.917 1.86 6.36 2.948 10.079 2.948 12.067 0 18.661-9.995 18.661-18.651 0-0.276 0-0.557-0.021-0.839 1.287-0.917 2.401-2.079 3.281-3.396z"></path>
                                 </svg>
                             </a> */}
-                            <a rel="noopener noreferrer" href="https://github.com/mjngit" className="flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 dark:bg-lime-400 dark:text-gray-900" style={{ paddingLeft: '2rem'}}>
+                            <a rel="noopener noreferrer" href="https://github.com/mjngit" className="flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 dark:bg-lime-400 dark:text-gray-900" style={{ paddingLeft: '8px'}}>
                                 <GitHubIcon />                               
                             </a>
 
