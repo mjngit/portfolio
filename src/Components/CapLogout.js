6c8df4f467cbb94365ee654126d9de2057ccbf9a
Mike Norris <mnorris6@elon.edu>
2023-12-07 23:44:52 -0500

    brute styling 1.68

--- src/Components/CapLogout.js
+++ src/Components/CapLogout.js
@@ -24,18 +24,32 @@ const CapLogout = ()=> {
     <div style={{display: 'flex', justifyContent:'center', alignItems: 'center'}}>
         {
         auth.id ? (
-            <div>
+            // <div>
                 
 
-                <div className='flex justify-center pt-20'>
-                <div className=" px-8 py-6 rounded-lg bg-gray-800 w-72">
-                <h1 className="text-center font-bold text-3xl text-white">Are You Sure?</h1>
-                <h4 className="text-center text-xl text-white py-5"> Logged in as {auth.username}</h4>
-                    <button onClick={(user)=> (_logout(user))} className="bg-blue-600 hover:bg-blue-500 text-white font-semibold p-2 mt-3 rounded w-[100%]">Logout</button>
+            //     <div className='flex justify-center pt-20'>
+            //     <div className=" px-8 py-6 rounded-lg bg-gray-800 w-72">
+            //     <h1 className="text-center font-bold text-3xl text-white">Are You Sure?</h1>
+            //     <h4 className="text-center text-xl text-white py-5"> Logged in as {auth.username}</h4>
+            //         <button onClick={(user)=> (_logout(user))} className="bg-blue-600 hover:bg-blue-500 text-white font-semibold p-2 mt-3 rounded w-[100%]">Logout</button>
                 
+            //     </div>
+            //     </div>
+            //     <div  className='py-40'></div>
+            // </div>
+
+            <div>
+                <div className='flex justify-center pt-20' style={{ display: 'flex', justifyContent: 'center', paddingTop: '5rem'}}>
+                <div className=" px-8 py-6 rounded-lg bg-gray-800 w-72" style={{ paddingLeft: '2rem', paddingRight: '2rem', paddingTop: '1.5rem', paddingBottom: '1.5rem', width: '18rem', backgroundColor: 'gray'}}>
+                    <h1 className="text-center font-bold text-3xl text-white" style={{textAlign: 'center', fontWeight: 'bold', fontSize: '1.875rem', lineHeight: '2.25rem' }}>Are You Sure?</h1>
+
+                    <h4 className="text-center text-xl text-white py-5"> Logged in as {auth.username}</h4>
+                        
+                    <button onClick={(user)=> (_logout(user))} className="bg-blue-600 hover:bg-blue-500 text-white font-semibold p-2 mt-3 rounded w-[100%]" style={{ backgroundColor: 'blue', color: 'white', fontWeight: 'bold', padding: '.5rem', marginTop: '.75rem', borderRadius: '.5rem'}}>Logout</button>
+
                 </div>
                 </div>
-                <div  className='py-40'></div>
+                <div  className='py-36' style={{ paddingTop: '12rem'}}></div>
             </div>
         )  : (
 
@@ -60,3 +74,7 @@ const CapLogout = ()=> {
 };
 
 export default CapLogout;
+
+
+     
+
